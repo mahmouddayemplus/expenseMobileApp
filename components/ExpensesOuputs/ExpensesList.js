a55b96a956fa22625429bf0db942ab7c3c9e60ca
mahmoud dayem <mahmoud.dayeem@gmail.com>
2025-07-05 21:46:35 +0300

    create expenseoutput components

---
+++ components/ExpensesOuputs/ExpensesList.js
@@ -0,0 +1,14 @@
+import { View, Text, FlatList } from 'react-native'
+import React from 'react'
+
+const ExpensesList = () => {
+    return (
+
+
+        <FlatList />
+
+
+    )
+}
+
+export default ExpensesList
